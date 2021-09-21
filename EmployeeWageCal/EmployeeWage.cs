9b096719d4ca5fd056c87624ceef85ae534246e6
mabelchristina <46992069+mabelchristina@users.noreply.github.com>
2021-09-21 07:39:05 +0530

    Update EmployeeWage.cs

--- EmployeeWageCal/EmployeeWage.cs
+++ EmployeeWageCal/EmployeeWage.cs
@@ -6,12 +6,12 @@ namespace EmployeeWageCal
 {
     class EmployeeWage
     {
-        const int IS_FULLTIME = 0, IS_PARTTIME = 1, WAGE_PER_HOUR = 20, FULL_DAY_HOUR = 8, PART_TIME_HOUR = 4;
+        const int IS_FULLTIME = 0, IS_PARTTIME = 1, WAGE_PER_HOUR = 20, FULL_DAY_HOUR = 8, PART_TIME_HOUR = 4, WORKDAY_PER_MONTH=20;
         int totalwage = 0, emphours = 0;
         public void EmployeeHours()
         {
             Random random = new Random();
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < WORKDAY_PER_MONTH; i++)
             {
                 int emplyoeecheck = random.Next(1, 3);
                 switch (emplyoeecheck)
